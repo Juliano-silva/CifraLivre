a2694a8c32c7caad91acb52fb4882499a98f6479
Juliano Almeida <88514570+Juliano-silva@users.noreply.github.com>
2025-08-27 16:24:49 -0300

    27/08/2025 - CREATE NavBar

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import render_template,Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def Home():
+    return render_template("Home.html")
+
+@app.route("/Home")
+def Index():
+    return render_template("index.html")
+
+@app.route("/add")
+def add():
+    return render_template("add_urls.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+if __name__ == "__main__":
+    app.run(port=5636,debug=True)
